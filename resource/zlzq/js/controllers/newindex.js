39d0343c31828dbf52a1211a42a95845e7f1e7b2
liuqiang <294007813@qq.com>
2015-08-17 15:17:58 +0800

    移植

---
+++ resource/zlzq/js/controllers/newindex.js
@@ -0,0 +1,20 @@
+define(['BaseView', "cUIInputClear","cUIImageSlider", "Model", "Store",], function (BaseView, cUIInputClear,cUIImageSlider, Model, Store) {
+    var self;
+    var View = BaseView.extend({
+        ViewName: 'newindex',
+        events: {
+            "click .iconBrush":"toDecorate",
+        },
+
+        //点击装修
+        toDecorate:function(){
+            Lizard.goTo("decoratelist.html");
+        },
+
+        onShow: function () {
+
+        },
+
+    });
+    return View;
+})
