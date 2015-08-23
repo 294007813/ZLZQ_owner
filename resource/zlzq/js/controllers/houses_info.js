6453f9f5d2e0302ceffa2a53f56081aaa1ff2ceb
yan_sun <sy_lnu@sina.com>
2015-08-23 22:07:49 +0800

    上传 获取房源信息

--- resource/zlzq/js/controllers/houses_info.js
+++ resource/zlzq/js/controllers/houses_info.js
@@ -3,20 +3,46 @@ define(['BaseView', "cUIInputClear","cUIImageSlider", "Model", "Store",], functi
     var View = BaseView.extend({
         ViewName: 'houses_info',
         events: {
-            "click .icon-person":"toPersonal",
-            "click .icon-home":"toLocation",
-            "click .back":"toMyhouse",
-            "click .next":"toChange",
+            "click .icon-person": "toPersonal",
+            "click .icon-home": "toLocation",
+            "click .back": "toMyhouse",
+            "click .next": "toChange"
         },
 
-        toMyhouse:function(){
+        toMyhouse: function () {
             Lizard.goTo("myhouses.html");
         },
+        getHouseInfo: function (cb) {
+            if (!self.user) {
+                cb && cb();
+                self.showMyToast("未登录", 1500);
+                return;
+            }
+            var url = Lizard.host + Lizard.apiUrl + "owners/" + self.user.actor_id + "/realties/my_owner_realties?auth_token=" + self.user.authentication_token;
+            $.ajax({
+                url: url,
+                type: "get",
+                success: function (data) {
+                    self.hideLoading();
+                    cb(data);
 
-        onShow: function () {
-            this.hideLoading();
+                },
+                error: function (e) {
+                    self.hideLoading();
+                    self.showMyToast("网络错误", 1000);
+
+
+                }
+            });
         },
-
+        onCreate: function () {
+            self = this;
+        },
+        onShow: function () {
+            self.getHouseInfo(function () {
+                self.hideLoading();
+            })
+        }
 
 
     });
